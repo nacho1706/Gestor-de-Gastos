53bb725c20827504d338a24d2480afb951effbbe
“Ignacio <ignacioalbarracin15@gmail.com>
2024-03-07 10:28:22 -0300

    Mejoras al agregado de montos. Generacion de colores para futuro uso

--- features.js
+++ features.js
@@ -1,6 +1,10 @@
+
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('form_amount');
     const montoList = document.getElementById('amount_list');
+    let arrayColors = [];
+    let indexColor = 0;
+
 
     form.addEventListener('submit', function(event) {
         event.preventDefault();
@@ -12,11 +16,20 @@ document.addEventListener('DOMContentLoaded', function() {
             return;
         }
         
+        // Agrega un color random a la pila 
+        arrayColors.push('#' + Math.floor(Math.random()*16777215).toString(16));
+
         // Agregar el monto a la lista
         const listItem = document.createElement('li');
         const circleDiv = document.createElement('div');
         circleDiv.classList.add('icono');
         
+        //Cambia el color del div
+        if(event){
+            circleDiv.style.backgroundColor = arrayColors[indexColor];
+            indexColor++;
+            }   
+
         listItem.innerHTML = `<strong>$${montoValue.toFixed(2)}</strong>`;
         
         listItem.insertBefore(circleDiv, listItem.firstChild);
@@ -26,3 +39,8 @@ document.addEventListener('DOMContentLoaded', function() {
         montoInput.value = '';
     });
 });
+
+
+function changeColor(className) {
+    element.style.backgroundColor = randomColor;
+}
